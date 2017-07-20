2a2b23de1def90ed9c57f4c37dd720bdf5c3aee1
Maia <maia.rt.46@gmail.com>
2017-07-20 18:05:38 -0500

    Carousel

--- src/assets/js/components/outfit.js
+++ src/assets/js/components/outfit.js
@@ -1,14 +1,14 @@
 const Outfit = () => {
 
-	const photoContainer = $('<section class="photo-container"></section>');
-	const photoCont = $('<div class="photo-container__cont"></div>');
-  // const divMsj  = $('<div class="cont_text"><h4>Bienvenida al App de Oechsle.<br>Oechsle es la Tienda por Departamento del Grupo Intercorp. Somos parte de un grupo empresarial comprometido con el desarrollo y bienestar de las familias Peruanas. </h4></div>')
-  // const imgPhoto = $('<img src="http://cdn.modalia.es/images/stories/2015/febrero/hm-chaquetas-ss15/hm-chaquetas-ss15_6.jpg" class="img-responsive" alt="chaqueta">');
-  // const photoFooter = $('<div class="photo-container__footer">'+
-  //                         '<div class="img-circle"><i class="glyphicon glyphicon-camera"></i></div>'+
-  //                       '</div>');
-  photoCont.append(divMsj);
-  photoContainer.append(photoCont);
-  photoContainer.append(photoFooter);
-	return photoContainer;
+  const secCarousel = $("<div class='owl-carousel owl-theme'>"+
+                      "<div class='item'><img src='assets/img/jean- 1.jpg'></div>"+
+                      "<div class='item'><img src='assets/img/jean-2.jpg'></div>"+
+                      "<div class='item'><img src='assets/img/pants-b1.jpg'></div>"+
+                      "<div class='tem'><img src='assets/img/pants-b2.jpg'></div>"+
+                      "<div class='item'><img src='assets/img/pants-be1.jpg'></div>"+
+                      "<div class='item'><img src='assets/img/pants-be2.jpg'></div>"+
+                      "<div class='item'><img src='assets/img/pants-n1.jpg'></div>"+
+                      "<div class='item'><img src='assets/img/pants-n2.jpg'></div>"+
+                      "</div>");
+  return secCarousel;
 }
