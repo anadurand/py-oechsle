53b88cf83721a496a10e6f13d32b5cc32a39c7a4
Maia <maia.rt.46@gmail.com>
2017-07-20 17:37:10 -0500

    Agregando la pagina central

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,37 +2,24 @@
 const render = (root)=>{
     root.empty();
     const section = $('<section class="components"></section>');
-    section.append(Header());
-    section.append(welcome());
+    state.cloth=1;
+    if (state.cloth == null){
+      section.append(Header());
+      section.append(welcome());
+    }else {
+      section.append(Outfit());
+    }
+
     // section.append(Footer());
     root.append(section);
 };
-// const state = {
-//     cloth: null,
-//     clothSelected : null
-// };
+const state = {
+    cloth: null,
+    clothSelected : null
+};
 
 $( _ => {
-
-    // $.get('',(json) => {
-    //     if(!json){
-    //       return alert("error");
-    //     }
-        // state.cloth = json;
-        // console.log(state.cloth);
         const root = $('#root');
         render(root);
-    // });
-
-        // var socket = io();
-        // $('form').submit(function(){
-        //   socket.emit('chat message', $('#m').val());
-        //   $('#m').val('');
-        //   return false;
-        // });
-        // socket.on('chat message', function(msg){
-        //   $('#messages').append($('<li>').text(msg));
-        //   window.scrollTo(0, document.body.scrollHeight);
-        // });
-
+        console.log(state.cloth);
 })
