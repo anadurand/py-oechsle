909f244ef3312f3fe964c32e1562af98a7a3d714
Cindy Mendoza <mendozaic@hotmail.com>
2017-07-20 13:45:14 -0500

    header

--- src/assets/js/components/header.js
+++ src/assets/js/components/header.js
@@ -1,10 +1,19 @@
 const Header = () => {
 
-	const headerContainer = $("<header class='header-container'></header>");
-	const search = $("<div class='header-container__search'>Hola soy el header</div>");
+	const headerContainer = $("<header class='container-fluid'>"+
+														"<div class='row'><div class='col-xs-2 col-sm-2'>"+
+														"<div class='header--icoHamburger'><i class='glyphicon glyphicon-menu-hamburger header--boxes-margin'></i></div>"+
+														"</div>"+
+														"<div class='col-xs-8 col-sm-8'><div class='header--message'><p class='header--boxes-margin'>Snap a picture</p></div></div>"+
+														"<div class='col-xs-2 col-sm-2'>"+
+														"<i class='glyphicon glyphicon-shopping-cart header--boxes-margin'></i>"+
+														"</div>"+
+														"</div></header>");
+
+	// const search = $("<div class=''></div>");
 
 
-	headerContainer.append(search);
+	// headerContainer.append(search);
 
 	return headerContainer;
 
