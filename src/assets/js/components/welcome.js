0d10ab27d3178a28790e1566ad0a2c82818657cc
Cindy Mendoza <mendozaic@hotmail.com>
2017-07-20 19:04:55 -0500

    pantalla carrito de comparas

--- src/assets/js/components/welcome.js
+++ src/assets/js/components/welcome.js
@@ -6,6 +6,6 @@ const Welcome = () => {
             "<img src='assets/img/logoOe.png' alt='LogoOe' height='100%' width='100%'>"+
             "</div></div></div>"+
             "</section>");
-            setTimeout(function(){ Header(); }, 1000);
+            // setTimeout(function(){ Header(); }, 1000);
   return secWelcome;
 }
