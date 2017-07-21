cee747bdf460dd77f78cb2a0400ca704753b58fd
Cindy Mendoza <mendozaic@hotmail.com>
2017-07-21 09:24:08 -0500

    adaptando maps y filter

--- src/assets/js/components/fittingRoom.js
+++ src/assets/js/components/fittingRoom.js
@@ -1,17 +1,176 @@
-const FittingRoom = () => {
-  const secFRoom = $("<section id='fittingRoom' class='container-fluid'><div class='row'>"+
+const FittingRoom = (update) => {
+  const secFRoom = $("<section id='fittingRoom' class='container-fluid'></section>");
+  const row = $("<div class='row'>"+
   "<div class='col-xs-2 col-sm-2 container-flex-row'><i class='glyphicon glyphicon-remove'></i></div>"+
-  "<div class='col-xs-10 col-sm-10 container-flex-row'><p>Fitting Room</p></div></div>"+
-  "<div class='container'><div class='row'><div class='col-xs-12 col-sm-12 container-flex-colum-flxStart'><p>select a location</p>"+
-  "<input type='text' name="" value="" class='fitRoom--input-class'>"+
-  "<div class="" id='mapa'></div><p>select a date and time</p><input type='text' name="" value="" class='fitRoom--input-class'>"+
-  "</div></div><div class='row'><div class='col-xs-6 col-sm-6 container-flex-row'><span class='fitRoom--span-class fitRoom--span-pd'>10:30 - 12:00</span>"+
-  "</div><div class='col-xs-6 col-sm-6 container-flex-row'><span class='fitRoom--span-class fitRoom--span-pd'>13:30 - 15:00</span>"+
-  "</div></div><div class='row'><div class='col-xs-6 col-sm-6 container-flex-row'><span class='fitRoom--span-class fitRoom--span-pd'>15:30 - 17:00</span>"+
-  "</div><div class='col-xs-6 col-sm-6 container-flex-row'><span class='fitRoom--span-class fitRoom--span-pd'>17:30 - 19:00</span></div>"+
-  "</div><div class='row'><div class='col-xs-12 col-sm-12 container-flex-row'>"+
-  "<button type='button' name='button' class='col-xs-12 col-sm-12 shopCart--button-class fitRoom--span-pd'>Book appointment</button>"+
-  "</div></div></div></section>");
+  "<div class='col-xs-10 col-sm-10 container-flex-row'><p>Fitting Room</p></div>"+
+  "</div>");
+  const conta = $("<div class='container'></div>");
+  const rowc = $("<div class='row'><div class='col-xs-12 col-sm-12 container-flex-colum-flxStart'></div></div>");
+  const div = $("<div></div>");
+  const p = $("<p>select a location</p>");
+  const inputFilter = $("<input type='text' class='fitRoom--input-class' placeholder='Ingresa tu tienda a buscar'>");
+  const mapa = $("<div id='mapa'></div>");
+  const div2 = $("<div><p>select a date and time</p>"+
+  "<input type='text' class='fitRoom--input-class'></div>");
+  const rows = $("<div class='row'><div class='col-xs-6 col-sm-6 container-flex-row'><span class='fitRoom--span-class fitRoom--span-pd'>10:30 - 12:00</span></div>"+
+    "<div class='col-xs-6 col-sm-6 container-flex-row'><span class='fitRoom--span-class fitRoom--span-pd'>13:30 - 15:00</span></div></div>"+
+    "<div class='row'><div class='col-xs-6 col-sm-6 container-flex-row'><span class='fitRoom--span-class fitRoom--span-pd'>15:30 - 17:00</span></div>"+
+    "<div class='col-xs-6 col-sm-6 container-flex-row'><span class='fitRoom--span-class fitRoom--span-pd'>17:30 - 19:00</span></div></div>"+
+    "<div class='row'><div class='col-xs-12 col-sm-12 container-flex-row'><button type='button' name='button' class='col-xs-12 col-sm-12 fitRoom--button-class fitRoom--span-pd'>Book appointment</button>"+
+    "</div></div>");
+
+secFRoom.append(row);
+secFRoom.append(conta);
+conta.append(rowc)
+rowc.append(div);
+div.append(p);
+div.append(inputFilter);
+div.append(mapa);
+rowc.append(div2);
+conta.append(rows);
+inputFilter.on('keyup',(e) => {
+  const find = filterByLocal(state.locals,inputFilter.val());
+
+  reRender (mapa, find, update);
+});
+const reRender = (mapa, find, update) => {
+ mapa.empty();
+ find.forEach((local)=>{
+   mapa.append(localItem(local,update,_=> {reRender(mapa, find);}));
+ });
+}
+const localItem = (local, update, reRender ) => {
+ const contLocal=  $('<div class="col-xs-12"></div>');
+ const contName= $('<div class="col-xs-9"></div>');
+ const name = $('<h5>'+ local["data-store"] +'</h5>');
+ const contLink = $('<div class="col-xs-3 center-align"></div>');
+ const mapIcon = $('<i class="glyphicon glyphicon-map-marker"></i>');
+ const contAddres = $('<div class="col-xs-12 item-local"></div>');
+ const address= $('<p>'+ local.address +'<br>'+ "-" + local["data-province"]+'</p>');
+
+ contName.append(name);
+ contLink.append(mapIcon);
+ contAddres.append(address);
+
+ contLocal.append(contName);
+ contLocal.append(contLink);
+ contLocal.append(contAddres);
+
+ mapIcon.on('click',(e) => {
+   e.preventDefault();
+   state.localSelected = local;
+   $("#mapa").empty();
+
+   initMap(local);
+ });
+
+ return contLocal;
+}
+
+function initMap(local) {
+ console.log(local);
+
+ const  map = new google.maps.Map(document.getElementById('mapa'), {
+   center: {lat: parseFloat(local.lat), lng: parseFloat(local.lng)},
+   zoom: 17
+ });
+
+ const marker = new google.maps.Marker({
+   map: map,
+   draggable: true,
+   animation: google.maps.Animation.DROP,
+   position: {lat: parseFloat(local.lat), lng:parseFloat(local.lng)}
+ });
+ marker.addListener('load', toggleBounce);
+
+ function toggleBounce() {
+   if (marker.getAnimation() !== null) {
+     marker.setAnimation(null);
+   } else {
+     marker.setAnimation(google.maps.Animation.BOUNCE);
+   }
+ }
+}
+
+
+const unSelect = (e)=>{
+  e.style.opacity="1";
+  ac = false;
+}
+const Select = (e)=>{
+  console.log("select" + e);
+  ac = true;
+  e.style.opacity="0.5";
+}
+let ac = false;
+$(".fitRoom--span-class").click((e)=>{
+  if(!ac){
+      Select(e.target);
+  }else{
+    unSelect(e.target);
+  }
+  console.log(e.target);
+});
 
   return secFRoom;
 }
+
+// const FittingRoom = (update) => {
+//  const container = $('<div class="container"></div>');
+//  const row = $('<div class="row "></div>');
+//  const iconSearch = $('<span  class="glyphicon glyphicon-search"></span>');
+//  const search = $('<input class="col s12" placeholder="Ingresa tu tienda a buscar"></input>');
+//  const rowContainer = $('<div class="row " id="locals"></div>');
+//
+//  row.append(iconSearch);
+//  row.append(search);
+//
+//  container.append(row);
+//  container.append(rowContainer);
+//
+//  search.on('keyup',(e) => {
+//    const find = filterByLocal(state.locals,search.val());
+//
+//    reRender (rowContainer, find, update);
+//  });
+//
+//  return container;
+// }
+
+// const reRender = (rowContainer, find, update) => {
+//  rowContainer.empty();
+//
+//  find.forEach((local)=>{
+//    rowContainer.append(localItem(local,update,_=> {reRender(rowContainer, find);}));
+//  });
+// }
+
+// const localItem = (local, update, reRender ) => {
+//  const contLocal=  $('<div class="col-xs-12"></div>');
+//
+//  const contName= $('<div class="col-xs-9"></div>');
+//  const name = $('<h5>'+ local["data-store"] +'</h5>');
+//  const contLink = $('<div class="col-xs-3 center-align"></div>');
+//  const mapIcon = $('<i class="glyphicon glyphicon-map-marker"></i>');
+//
+//  const contAddres = $('<div class="col-xs-12 item-local"></div>');
+//  const address= $('<p>'+ local.address +'<br>'+ "-" + local["data-province"]+'</p>');
+//
+//  contName.append(name);
+//  contLink.append(mapIcon);
+//  contAddres.append(address);
+//
+//  contLocal.append(contName);
+//  contLocal.append(contLink);
+//  contLocal.append(contAddres);
+//
+//  mapIcon.on('click',(e) => {
+//    e.preventDefault();
+//    state.localSelected = local;
+//    $("#locals").empty();
+//
+//    initMap(local);
+//  });
+//
+//  return contLocal;
+// }
